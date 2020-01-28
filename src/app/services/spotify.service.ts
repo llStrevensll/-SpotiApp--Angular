451dfe37e93d69a9ba9c6e41b4d46c6d3e28b81c
Steven <anguelr12@hotmail.com>
2020-01-28 15:09:56 -0500

    Centralizar las peticiones hacia Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,25 +12,30 @@ export class SpotifyService {
     console.log('Spotify Service Listo');
     
   }
-  
-  //Obtener nuevas canciones(estrenos)
-  getNewReleases(){
+  //Obtener query-> para busquedas
+  getQuery(query: string){
+
+    const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQCelRNclAwBmAk28lDHLC0_vN3_yfKJ_P9Fo4kzPX_fQYmdOYj6Vh_d9-e8KgB7-OEzITtEPz3VCxKbz5U'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers})
-                .pipe( map( data => data['albums'].items));//map -> Observable
-          
+
+    return this.http.get(url, {headers}); //retorna el observable
+  }
+  
+  //Obtener nuevas canciones(estrenos)
+  getNewReleases(){
+    
+    return this.getQuery('browse/new-releases?limit=20')
+                .pipe( map( data => data['albums'].items));//map -> Observable      
   }
 
   //Obtener Artistas
   getArtista(termino: string){
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCelRNclAwBmAk28lDHLC0_vN3_yfKJ_P9Fo4kzPX_fQYmdOYj6Vh_d9-e8KgB7-OEzITtEPz3VCxKbz5U'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=25`, {headers})
-              .pipe(map(data =>  data['artists'].items));
+
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=25`)
+               .pipe(map(data =>  data['artists'].items));
   
   }
 }

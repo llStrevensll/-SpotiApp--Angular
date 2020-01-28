67b5a85e7fe47a07b0b797e24713399b03e95d02
Steven <anguelr12@hotmail.com>
2020-01-28 14:16:40 -0500

    Consumiendo info - Ver Canciones y artistas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -16,10 +16,8 @@ export class SpotifyService {
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQBq5C_CyFvhyw_qTIgwhBUD7A90r96EqzZxtUQUPzWOTJCBB95T4B3QWFRxiu-yXzHCDscspccJZ_4LuGY'
     });
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=10', {headers})
-        .subscribe(data => {//data desde spotify
-          console.log(data);
-          
-        });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers});
+    
+  
   }
 }
